06b49b84342705814b9bc921bdd8660746cb710e
JoseAG19 <165836565+JoseAG19@users.noreply.github.com>
2024-04-30 14:25:15 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("BIENVENIDOS AL NUEVO MUNDO DE LAS CIUDADES INTELIGENTES")
 st.subheader("Elaborado por José Alejandro Guerra - Diseñador Urbano")
 st.image("PORTADA.jpg")
+st.text("El 70% de la población vivirá en ciudades para el año 2030; es por eso que requerimos mayor capacidad instalada para atender y planificar los retos urbanos del futuro, y así, tener ciudades más justas y saludables.")

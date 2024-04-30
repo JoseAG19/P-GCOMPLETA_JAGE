dba3c7c156268a03f12e956b2ff3fead973c15c6
JoseAG19 <165836565+JoseAG19@users.noreply.github.com>
2024-04-30 14:17:00 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("BIENVENIDOS AL NUEVO MUNDO DE LAS CIUDADES INTELIGENTES")
-st.subheader("Elaborado por José Alejandro Guerra")
-
+st.subheader("Elaborado por José Alejandro Guerra - Diseñador Urbano")
+st.image("PORTADA.jpg)
